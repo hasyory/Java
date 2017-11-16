8ab30d8c213a2562783c7eb6969b3fa745f5fe49
hgyung <h0ldsu9arp1z@gmail.com>
2017-11-16 18:15:37 +0900

    Step01

---
+++ Step01_Comment/src/test/main/MainClass.java
@@ -0,0 +1,20 @@
+package test.main;
+//  한줄 주석입니다.
+//  역시 한줄 주석입니다.
+/* 
+ *	빨간색으로 표시된 글자는 예약어 -> 식별자로 쓸 수 없다.
+ * 예를 들어 class명을 static이나 void로 지정할 수 없다.
+ */
+
+public class MainClass {//  클래스명 : MainClass, 파일명과 클래스명은 같아야함!
+	
+	//  실행순서가 시작되는 특별한 main 메소드 (method), javascript의 function main(args){}와 비슷한 의
+	public static void main(String[] args) {
+		
+		System.out.println("안녕하세요."); // syso 입력 후 ctrl + space
+		System.out.println("이제 시작이군요.");
+		System.out.println("main 메소드가 리턴(종료)됩니다.");
+		
+	}
+
+}
